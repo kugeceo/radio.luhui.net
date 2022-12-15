b12128c6a9d84c6ab3139291ff32ccafcf260b8b
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2022-12-15 11:18:55 +0800

    Add files via upload

---
+++ assets/static-not-found-legacy.87c34165.js
@@ -0,0 +1,1 @@
+System.register(["./modulepreload-polyfill-legacy.ed506caa.js","./static-page-legacy.a174087d.js"],(function(){"use strict";return{setters:[()=>{},()=>{}],execute:function(){}}}));
---
+++ assets/static-page-legacy.a174087d.js
@@ -0,0 +1,1 @@
+System.register([],(function(){"use strict";return{execute:function(){const e=window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light";document.documentElement.dataset.theme=e}}}));
